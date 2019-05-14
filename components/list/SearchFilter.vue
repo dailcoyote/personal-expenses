b436a0d721b45c073d3e30f35920c2c742f10674
dailcoyote <dailcoyote@gmail.com>
2019-05-15 01:48:40 +0600

    +new: SearchFilter UI Component

---
+++ components/list/SearchFilter.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-layout row wrap justify-center mb-3>
+    <v-flex lg6 md-4 sm12 xs12>
+      <v-card class="elevation-0">
+        <v-card-text class="pa-0">
+          <v-list two-line class="pa-0">
+            <template>
+              <v-list-tile avatar @click="onSearchOpen">
+                <v-list-tile-content color="brown lighten-2">
+                  <v-list-tile-sub-title class="subtitle">
+                    <span style="padding-left:25px;">Search Filter</span>
+                  </v-list-tile-sub-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                  <v-icon dark color="brown lighten-2">filter_list</v-icon>
+                </v-list-tile-action>
+              </v-list-tile>
+            </template>
+          </v-list>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  methods: {
+    onSearchOpen: e => {
+      console.log(e);
+    }
+  }
+};
+</script>
+
--- pages/activities.vue
+++ pages/activities.vue
@@ -1,11 +1,23 @@
 <template>
   <div id="pageActivityList">
     <v-container grid-list-lg fluid>
-      <v-layout row wrap ma-0 align-center justify-center mb-3
-                v-for="(activity, rootIndx) in activities" :key="rootIndx">
+      <search-filter></search-filter>
+      <v-layout
+        row
+        wrap
+        ma-0
+        align-center
+        justify-center
+        mb-3
+        v-for="(activity, rootIndx) in activities"
+        :key="rootIndx"
+      >
         <v-flex lg6 md-4 sm12 xs12>
-          <activity-card-list v-bind:title="activity.date" v-bind:items="activity.listOf" 
-                              :currency="currency"></activity-card-list>
+          <activity-card-list
+            v-bind:title="activity.date"
+            v-bind:items="activity.listOf"
+            :currency="currency"
+          ></activity-card-list>
         </v-flex>
       </v-layout>
     </v-container>
@@ -20,11 +32,13 @@
 import Moment from "moment";
 import settings from "@/settings";
 import activities from "@/api/activities";
+import SearchFilter from "@/components/list/SearchFilter";
 import ActivityCardList from "@/components/widgets/ActivityCardList";
 
 export default {
   layout: "dashboard",
   components: {
+    SearchFilter,
     ActivityCardList
   },
   data: () => ({
