3258ac11651c2e10f232a67d187771883fda22c7
dailcoyote <dailcoyote@gmail.com>
2019-05-15 02:11:14 +0600

    +new > SearchFilterDialog layout

--- components/list/SearchFilter.vue
+++ components/list/SearchFilter.vue
@@ -5,7 +5,7 @@
         <v-card-text class="pa-0">
           <v-list two-line class="pa-0">
             <template>
-              <v-list-tile avatar @click="onSearchOpen">
+              <v-list-tile avatar @click="onSearchOpen()">
                 <v-list-tile-content color="brown lighten-2">
                   <v-list-tile-sub-title class="subtitle">
                     <span style="padding-left:25px;">Search Filter</span>
@@ -20,14 +20,39 @@
         </v-card-text>
       </v-card>
     </v-flex>
+    <!--        D I A L O G     -->
+    <v-dialog
+      v-model="search.dialog"
+      fullscreen
+      transition="dialog-bottom-transition"
+      :overlay="false"
+    >
+      <v-card>
+        <v-toolbar dark color="#40668e">
+          <v-btn icon @click.native="search.dialog = false" dark>
+            <v-icon>close</v-icon>
+          </v-btn>
+          <v-toolbar-title>Search Filter</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn dark flat @click.native="search.dialog = false">Save</v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+      </v-card>
+    </v-dialog>
   </v-layout>
 </template>
 
 <script>
 export default {
+  data: () => ({
+      search: {
+        dialog: false
+      }
+  }),
   methods: {
-    onSearchOpen: e => {
-      console.log(e);
+    onSearchOpen: function() {
+      this.search.dialog = true;
     }
   }
 };
