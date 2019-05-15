eb62a016daf5ff3fab5aace68d7c630274a37e76
dailcoyote <dailcoyote@gmail.com>
2019-05-16 03:32:54 +0600

    Fixes

--- pages/activities.vue
+++ pages/activities.vue
@@ -1,32 +1,34 @@
 <template>
   <div id="pageActivityList">
-    <v-container grid-list-lg fluid v-if="!activitiesLoading && activities">
-      <search-filter></search-filter>
-      <v-layout
-        row
-        wrap
-        ma-0
-        align-center
-        justify-center
-        mb-3
-        v-for="(activity, rootIndx) in activities"
-        :key="rootIndx"
-      >
-        <v-flex lg6 md-4 sm12 xs12>
-          <activity-card-list
-            v-bind:title="activity.date"
-            v-bind:items="activity.listOf"
-            :currency="currency"
-          ></activity-card-list>
-        </v-flex>
+    <v-container>
+      <v-container grid-list-lg fluid v-if="!activitiesLoading && activities">
+        <search-filter></search-filter>
+        <v-layout
+          row
+          wrap
+          ma-0
+          align-center
+          justify-center
+          mb-3
+          v-for="(activity, rootIndx) in activities"
+          :key="rootIndx"
+        >
+          <v-flex lg6 md-4 sm12 xs12>
+            <activity-card-list
+              v-bind:title="activity.date"
+              v-bind:items="activity.listOf"
+              :currency="currency"
+            ></activity-card-list>
+          </v-flex>
+        </v-layout>
+      </v-container>
+      <v-layout v-else row wrap align-center justify-center ma-0 pb-4>
+        <v-progress-circular :size="60" color="primary" indeterminate ma-auto></v-progress-circular>
       </v-layout>
+      <v-btn color="#40668e" dark fixed bottom right fab :style="moveForFabButtonStyle">
+        <v-icon>add</v-icon>
+      </v-btn>
     </v-container>
-    <v-layout v-else row wrap align-center justify-center ma-0 pb-4>
-      <v-progress-circular :size="60" color="primary" indeterminate ma-auto></v-progress-circular>
-    </v-layout>
-    <v-btn color="#40668e" dark fixed bottom right fab :style="moveForFabButtonStyle">
-      <v-icon>add</v-icon>
-    </v-btn>
   </div>
 </template>
 
@@ -34,7 +36,6 @@
 import { mapState } from "vuex";
 import Moment from "moment";
 import settings from "@/settings";
-// import activities from "@/api/activities";
 import SearchFilter from "@/components/list/SearchFilter";
 import ActivityCardList from "@/components/widgets/ActivityCardList";
 
